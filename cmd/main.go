bb727278a028eade2f714dfacf25c30130ee44ac
Shohel-Raj <shohel87.dev@gmail.com>
2026-06-28 06:30:03 +0600

    basic jwt and  database connected

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"spotssync/internal/config"
+	"spotssync/internal/server"
+)
+
+func main() {
+	// load environment variables
+	cfg := config.LoadEnv()
+	// connect to the database
+	db := config.ConnectDatabase(cfg)
+	// start the server
+	server.Start(db, cfg)
+
+}
--- internal/server/http.go
+++ internal/server/http.go
@@ -1,1 +1,42 @@
 package server
+
+import (
+	"fmt"
+	"spotssync/internal/config"
+
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v5"
+	"gorm.io/gorm"
+)
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	if err := cv.validator.Struct(i); err != nil {
+		// Optionally, you could return the error to give each route more control over the status code
+		return echo.ErrBadRequest.Wrap(err)
+	}
+	return nil
+}
+
+func Start(db *gorm.DB, cfg *config.Config) {
+
+	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	e.GET("/health", func(c *echo.Context) error {
+		return c.String(http.StatusOK, "server is running")
+	})
+
+	//routes
+	// user.RegisterRoutes(e, db, cfg)
+
+	port := fmt.Sprintf(":%s", cfg.Port)
+	if err := e.Start(port); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
